f8e18f8a4b9f262d2d63f4852ccfee9289253caf
Baraciak <baraciak@gmail.com>
2019-07-01 11:56:48 +0200

    Repo initialized

---
+++ src/main/java/Gameplay.java
@@ -0,0 +1,8 @@
+class Gameplay {
+    public static void main(String[] args) {
+        Player player1 = new Player("X");
+        Player player2 = new Player("O");
+
+        Board board = new Board(3, 3);
+    }
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,8 @@
+class Player {
+
+    String name;
+
+    Player(String name, String character){
+
+    }
+}
