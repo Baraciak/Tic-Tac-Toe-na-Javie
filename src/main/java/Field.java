58e40977d6fc94d80c1970c8b890768e6afedaec
Daria Rzepka <dariarz0110@gmail.com>
2019-07-02 14:46:32 +0200

    Field class: checking if field is empty and setting value method

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,8 +1,19 @@
 class Field {
     String value;
 
-    Field(String fieldValue){
+    Field(String fieldValue) {
         value = fieldValue;
     }
 
+
+    //checks is box is empty. If it is empty returns true
+    boolean check(Field field) {
+        return (this.value == " ");
+
+
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
